4a5cc38006989e7c08dacab6a626add2172d73cb
gn-exia <djamelmiloudi3@gmail.com>
2020-05-27 09:54:33 +0200

    add feature login/logout

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,16 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+  $response = $PDO->query("SELECT * FROM user WHERE nickname = '$username' and password = '$password'");
+  $users = $response->fetchAll();
+  if (empty($users)) {
+    return -1;
+  } else {
+    $oneUser = $users[0];
+    return $oneUser['id'];
+  }
+}
